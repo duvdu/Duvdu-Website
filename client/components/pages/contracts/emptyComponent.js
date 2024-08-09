fcbdb7c5dfcd84042ecdaa97a0d550ad1b935294
Ahmed magdy <ahmedmegaa1@gmail.com>
2024-08-09 04:34:48 +0300

    Empty Component Card #159

--- client/components/pages/contracts/emptyComponent.js
+++ client/components/pages/contracts/emptyComponent.js
@@ -1,7 +1,7 @@
 const EmptyComponent = ({message}) => {
     return (
         <div className="px-4 sm:px-0">
-        <div className='flex flex-col justify-center items-center text-center w-full h-NoProjectYet border-NoProjectYet h-body p-10'>
+        <div className='flex flex-col justify-center items-center text-center w-full h-NoProjectYet border border-dashed rounded-[45px] border-opacity-25 border-black dark:border-[#ffffff50] h-body p-10'>
             <div className='bg-gray-600 mt-5' />
             <img src='/assets/imgs/theme/Empty.svg' className='h-full lg:w-[500px] lg:max-h-[450px]' />
             <h3 className='text-2xl font-bold mt-8 mb-4'>{message}</h3>
