e589fe0f1ef8d6784fbc07ead95cb19f2332027d
Ahmed magdy <ahmedmegaa1@gmail.com>
2024-08-10 05:06:35 +0300

    lasts

--- client/components/elements/duvduLoading.js
+++ client/components/elements/duvduLoading.js
@@ -1,7 +1,6 @@
 import { connect } from "react-redux";
 
 const DuvduLoading = ({ loadingIn, api, test = false }) => {
-    console.log(api)
     return (
         // <img className={ + "load  transition duration-500 ease-in-out"} src="/assets/imgs/loading.gif" alt="loading" />
         <>
