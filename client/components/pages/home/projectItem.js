19437b4538f9d57df7c784cf454b65735ea9bd78
mos3addev <mos3addev@gmail.com>
2024-11-19 19:05:12 +0200

    autoplay muted playsinline in video

--- client/components/pages/home/projectItem.js
+++ client/components/pages/home/projectItem.js
@@ -144,7 +144,8 @@ const ProjectItem = ({ cardData: initialCardData,inclusive, className = "", type
                                 <Link href={`/${type}/${cardData?._id}`}>
                                     <a>
                                         <video
-                                            autoplay muted playsinline
+                                            autoPlay muted playsInline
+                                            // autoplay muted playsinline
                                             className='cardvideo h-full'
                                             ref={videoRef}
                                             id={cardData?._id}
