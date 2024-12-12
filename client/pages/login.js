89279b3dec5ea2522d7fda125471ec2cc5272c08
AhmedMos3ad <117324729+Mos3addev@users.noreply.github.com>
2024-12-12 21:32:50 +0200

    Update login.js

--- client/pages/login.js
+++ client/pages/login.js
@@ -36,7 +36,7 @@ function Login({ api, login_respond , getMyprofile_respond, login, resendCode, g
     if (login_respond?.message === 'success') {
       setProfileLoading(true)
       getMyprofile().then(() => {
-        if ((document.referrer.startWith('/register')|| document.referrer==='/register' || document.referrer === '/forgetPassword' || document.referrer === '/login' )) {
+        if ((document.referrer.startsWith('/register')|| document.referrer==='/register' || document.referrer === '/forgetPassword' || document.referrer === '/login' )) {
           router.push('/');
         } else {
           router.back();
