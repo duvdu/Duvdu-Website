2777e6e150b75c54d2fc41e62c3c0b79daef4601
Ahmed Saad <70177377+a7mds3d2001@users.noreply.github.com>
2025-06-12 01:28:05 +0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,31 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
     BASE_URL: "https://api.duvdu.com/",
     ASSETS_URL: "https://duvdu-s3.s3.eu-central-1.amazonaws.com/",
-    DEFULT_PROFILE_PATH: "/assets/imgs/profile/defultUser.jpg",
-    DEFULT_COVER_PATH: "/assets/imgs/projects/cover.jpeg",
-    MAP_KEY: "AIzaSyD7vp9AFu2PiRWiNPkGtcrBKp9EMm-dFbQ",
+    DEFAULT_PROFILE_PATH: "/assets/imgs/profile/defaultUser.jpg",
+    DEFAULT_COVER_PATH: "/assets/imgs/projects/cover.jpeg",
+    MAP_KEY: "AIzaSyD7vp9AFu2PiRWNiPktcrBk9PEMn-dFbQ",
   },
-}
+
+  async headers() {
+    return [
+      {
+        // Match the AASA file path
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+          {
+            key: "Content-Disposition",
+            value: "inline",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
